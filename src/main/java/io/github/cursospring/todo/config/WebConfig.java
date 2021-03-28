c97026cab267522f49a5074504be9e8f97a8d43d
Rebeca Farias <fariasrebecac@gmail.com>
2021-03-28 01:28:50 -0300

    att

--- src/main/java/io/github/cursospring/todo/config/WebConfig.java
+++ src/main/java/io/github/cursospring/todo/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("https://rebecacfarias.github.io/todo-angular-frontend/");
+        registry.addMapping("/**");
     }
 }
